aebb9ad9b5dd684193286d60643bb246cc162a32
FI$H 2000 <fish2000@gmail.com>
2011-10-21 02:34:40 -0400

    Version bump to 0.2.0

--- delegate/__init__.py
+++ delegate/__init__.py
@@ -8,7 +8,7 @@ Copyright (c) 2011 Objects in Space and Time. All rights reserved.
 
 """
 __author__ = 'Alexander Bohn'
-__version__ = (0, 1, 8)
+__version__ = (0, 2, 0)
 
 import types
 from django.db import models
