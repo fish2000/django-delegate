7397165f201f837c2f5961e27715eddba2d729c7
Alexander Böhn <fish2000@gmail.com>
2014-01-19 19:22:59 -0500

    Bumped minor version number

--- version.py
+++ version.py
@@ -9,7 +9,7 @@ Copyright (c) 2011 Objects in Space and Time. All rights reserved.
 """
 __author__ = 'Alexander Bohn'
 __email__ = 'fish2000@gmail.com'
-__version__ = (0, 2, 7)
+__version__ = (0, 3, 0)
 __doc__ = '''
 Automatic delegate methods for Django managers and querysets,
 without runtime dispatch penalties.
